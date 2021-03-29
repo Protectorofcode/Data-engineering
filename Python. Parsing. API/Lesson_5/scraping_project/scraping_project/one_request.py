e7feef2874c84f785742e6bbe4586633abab5088
Protector20 <dima.burkin96@yandex.ru>
2021-03-29 22:20:07 +0300

    some update

--- Python. Parsing. API/Lesson_5/scraping_project/scraping_project/one_request.py
+++
@@ -1,9 +0,0 @@
-import time
-import requests
-
-url = 'https://omsk.hh.ru/search/vacancy?area=&fromSearchLine=true&st=searchVacancy&text=python'
-start = time.time()
-requests.get(url)
-end = time.time()
-delta = end - start
-print(delta)
--- Python. Parsing. API/Lesson_5/scraping_project/scraping_project/runner.py
+++ Python. Parsing. API/Lesson_5/scraping_project/scraping_project/runner.py
@@ -2,16 +2,15 @@ from scrapy.crawler import CrawlerProcess
 from scrapy.settings import Settings
 
 from jobparser.spiders.hhru import HhruSpider
-# from jobparser.spiders.sjru import SjruSpider
+from jobparser.spiders.superjoberu import SuperjobRuSpider
 from jobparser import settings
 
 
 if __name__ == "__main__":
     crawler_settings = Settings()
     crawler_settings.setmodule(settings)
-
     process = CrawlerProcess(settings=crawler_settings)
-    process.crawl(HhruSpider)
-    # process.crawl(SjruSpider)
 
+    process.crawl(HhruSpider,vacancy = 'Python')
+    process.crawl(SuperjobRuSpider,vacancy = 'Python')
     process.start()
