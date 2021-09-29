1e64f5f8151b11e7e89b54eb1b079cdfc4507c57
Protector20 <dima.burkin96@yandex.ru>
2021-09-29 23:52:07 +0300

    Add NoSQL Db

---
+++ Tarantool, Redis, MongoDB/Tarantool/ad.py
@@ -0,0 +1,21 @@
+import redis
+from flask import Flask, render_template, make_response, url_for, request
+from time import strftime
+
+app = Flask(__name__)
+r =  redis.Redis(host='localhost', port=6379, db=0)
+
+@app.route('/')
+def cooockies():
+    res = make_response(f"<h1>Вы уже были на сайте</h1>logged")
+    res.set_cookie("logged", "yes")
+    ret = request.cookies.get('logged')
+    day=strftime("%Y-%m-%d")
+    
+    if ret:
+        numb = str(r.get('page:index:counter:' + day))
+        return f'Вы уже были, ваш номер на {day} = {numb}'
+        
+    else:
+        r.incr('page:index:counter:' + day)
+        return '42 - ' + str(r.get('page:index:counter:' + day))
